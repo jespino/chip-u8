2ed741ba283f3477018b4c2134a140e8a6fc2f16
Jesús Espino <jespinog@gmail.com>
2016-03-31 22:26:00 +0200

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 extern crate rand;
 extern crate sdl2;
+extern crate docopt;
 
 mod cpu;
 mod ui;
 
+use docopt::Docopt;
+use std::env;
+
+const USAGE: &'static str = "
+Usage: chip-u8 <rom>
+";
 
 fn main() {
     let mut cpu = cpu::ChipU8::new();
-    cpu.load("pong");
+    let args = Docopt::new(USAGE)
+                      .and_then(|d| d.argv(env::args().into_iter()).parse())
+                      .unwrap_or_else(|e| e.exit());
+
+    cpu.load(args.get_str("<rom>"));
 
     loop {
         cpu.cycle();
